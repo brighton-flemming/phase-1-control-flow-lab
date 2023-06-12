766791fc3ab3d1a1b16fc30e824278bbeadc5159
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-06-12 22:25:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet() {
+  if (feet <= 400) {
+    return "This one is on me";
+  } else if (199 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks";
+  } else(feet > 2500);{
+    return "No can do";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity() {
+  if ('The city is NYC') {
+    return 'Ok, sounds good.';
+   } else ( 'The city is not NYC');{
+    return 'No go.';}
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip() {
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'is not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      console.log ('Bye');
+      break;
+  }
+
 }
